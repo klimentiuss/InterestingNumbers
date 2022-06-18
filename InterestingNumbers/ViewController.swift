f8de54ab9ab1d486a01bf59d144d3b30d74de89c
Danil Klimenko <klimenko.apple01@icloud.com>
2022-06-18 20:34:15 +0300

    Initial Commit

---
+++ InterestingNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterestingNumbers
+//
+//  Created by Daniil Klimenko on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
